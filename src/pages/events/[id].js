1fae682974e03e2321d829e34e51d2f7058f3f42
mazyogz <yogiprass11@gmail.com>
2024-01-10 17:24:36 +0700

    order page

--- src/pages/events/[id].js
+++ src/pages/events/[id].js
@@ -1,18 +1,16 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
-import Cookies from "js-cookie";
 
 function EventDetail() {
   const [eventDetail, setEventDetail] = useState(null);
   const [isModalOpen, setModalOpen] = useState(false);
   const router = useRouter();
   const [selectedTicketId, setSelectedTicketId] = useState(null);
-  const eventId = router.query.id; 
+  const eventId = router.query.id;
 
   useEffect(() => {
     const fetchData = async () => {
-
       try {
         if (eventId) {
           const response = await fetch(
@@ -27,7 +25,7 @@ function EventDetail() {
     };
 
     fetchData();
-  }, [eventId]); 
+  }, [eventId]);
 
   const toggleModal = (ticketId) => {
     setModalOpen(!isModalOpen);
@@ -38,16 +36,16 @@ function EventDetail() {
     try {
       if (eventId && selectedTicketId) {
         let accessToken = null;
-  
+
         const cookiesArray = document.cookie.split("; ");
         const accessTokenCookie = cookiesArray.find((cookie) =>
           cookie.startsWith("accessToken=")
         );
-  
+
         if (accessTokenCookie) {
           accessToken = accessTokenCookie.split("=")[1];
         }
-  
+
         const response = await fetch(
           `https://backend-app-ticketing-v12-production.up.railway.app/v1/api/order/${eventId}/${selectedTicketId}`,
           {
@@ -56,11 +54,10 @@ function EventDetail() {
               Authorization: `Bearer ${accessToken}`,
               "Content-Type": "application/json",
             },
-            body: JSON.stringify({
-            }),
+            body: JSON.stringify({}),
           }
         );
-  
+
         if (response.ok) {
           console.log("Order placed successfully!");
         } else {
@@ -111,19 +108,19 @@ function EventDetail() {
                     <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
                       <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                         <div className="sm:flex sm:items-start">
-                          <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                          <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-green-100 sm:mx-0 sm:h-10 sm:w-10">
                             <svg
-                              className="h-6 w-6 text-red-600"
+                              xmlns="http://www.w3.org/2000/svg"
                               fill="none"
                               viewBox="0 0 24 24"
                               stroke-width="1.5"
                               stroke="currentColor"
-                              aria-hidden="true"
+                              class="w-6 h-6"
                             >
                               <path
                                 stroke-linecap="round"
                                 stroke-linejoin="round"
-                                d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
+                                d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
                               />
                             </svg>
                           </div>
