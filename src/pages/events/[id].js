1b5527300006db076135237ecd64503c50b8abdd
mazyogz <110655233+mazyogz@users.noreply.github.com>
2024-10-03 16:35:50 +0700

    Update [id].js

--- src/pages/events/[id].js
+++ src/pages/events/[id].js
@@ -16,18 +16,14 @@ function EventDetail() {
   const [isModalOpen, setModalOpen] = useState(false);
   const router = useRouter();
   const [selectedTicketId, setSelectedTicketId] = useState(null);
-  const [successDialogOpen, setSuccessDialogOpen] = useState(false); // Tambahkan state untuk dialog sukses
+  const [successDialogOpen, setSuccessDialogOpen] = useState(false);
   const cancelButtonRef = useRef(null);
   const eventId = router.query.id;
 
   const today = new Date();
-
-  // Mengambil informasi tanggal
   const day = today.getDate();
-  const month = today.getMonth() + 1; // Ingat bahwa bulan dimulai dari 0, sehingga perlu ditambah 1
+  const month = today.getMonth() + 1;
   const year = today.getFullYear();
-
-  // Format tanggal sesuai kebutuhan (contoh: DD/MM/YYYY)
   const formattedDate = `${day}/${month}/${year}`;
 
   const formatDate = (inputDate) => {
@@ -40,7 +36,7 @@ function EventDetail() {
       try {
         if (eventId) {
           const response = await fetch(
-            `backend-app-ticketing-v12-production-7d84.up.railway.app/v1/api/events/${eventId}`
+            `https://backend-app-ticketing-v12-production-7d84.up.railway.app/v1/api/events/${eventId}`
           );
           const data = await response.json();
           setEventDetail(data);
@@ -57,13 +53,11 @@ function EventDetail() {
     setModalOpen(!isModalOpen);
     setSelectedTicketId(ticketId);
   };
-  console.log(eventDetail);
 
   const handleAgree = async () => {
     try {
       if (eventId && selectedTicketId) {
         let accessToken = null;
-
         const cookiesArray = document.cookie.split("; ");
         const accessTokenCookie = cookiesArray.find((cookie) =>
           cookie.startsWith("accessToken=")
@@ -79,7 +73,7 @@ function EventDetail() {
         }
 
         const response = await fetch(
-          `backend-app-ticketing-v12-production-7d84.up.railway.app/v1/api/order/${eventId}/${selectedTicketId}`,
+          `https://backend-app-ticketing-v12-production.up.railway.app/v1/api/order/${eventId}/${selectedTicketId}`,
           {
             method: "POST",
             headers: {
@@ -94,11 +88,7 @@ function EventDetail() {
           setSuccessDialogOpen(true);
           console.log("Order placed successfully!");
         } else {
-          console.error(
-            "Failed to place order:",
-            response.status,
-            response.statusText
-          );
+          console.error("Failed to place order:", response.status, response.statusText);
         }
       }
     } catch (error) {
@@ -109,8 +99,6 @@ function EventDetail() {
   if (!eventDetail) {
     return <div>Loading...</div>;
   }
-  console.log(eventDetail);
-  console.log(eventDetail.data.ticketData);
 
   return (
     <>
@@ -118,20 +106,16 @@ function EventDetail() {
       <div className="bg-white">
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <h1 className="text-3xl font-bold tracking-tight text-gray-900">
-            Events Detail
+            Events Details
           </h1>
 
           <div className="mt-2 border-b border-gray-200 pb-5 text-sm sm:flex sm:justify-between">
             <dl className="flex">
               <dt className="text-gray-500">Event number&nbsp;</dt>
-              <dd className="font-medium text-gray-900">
-                {eventDetail.data.eventData.id}
-              </dd>
+              <dd className="font-medium text-gray-900">{eventDetail.data.eventData.id}</dd>
               <dt>
                 <span className="sr-only">Date</span>
-                <span className="mx-2 text-gray-400" aria-hidden="true">
-                  &middot;
-                </span>
+                <span className="mx-2 text-gray-400" aria-hidden="true">&middot;</span>
               </dt>
               <dd className="font-medium text-gray-900">
                 <time>{formatDate(eventDetail.data.eventData.date)}</time>
@@ -164,19 +148,11 @@ function EventDetail() {
                 <div className="sm:col-span-12 md:col-span-7">
                   <dl className="grid grid-cols-1 gap-y-8 border-b border-gray-200 py-4 sm:grid-cols-2 sm:gap-x-6 sm:py-4 md:py-4">
                     <div>
-                      <dt className="font-medium text-gray-900">
-                        Waktu dan Tempat
-                      </dt>
+                      <dt className="font-medium text-gray-900">Waktu dan Tempat</dt>
                       <dd className="mt-3 text-gray-500">
-                        <span className="block">
-                          {eventDetail.data.eventData.venue}
-                        </span>
-                        <span className="block">
-                          {eventDetail.data.eventData.jam_mulai} - Selesai
-                        </span>
-                        <span className="block">
-                          {formatDate(eventDetail.data.eventData.date)}
-                        </span>
+                        <span className="block">{eventDetail.data.eventData.venue}</span>
+                        <span className="block">{eventDetail.data.eventData.jam_mulai} - Selesai</span>
+                        <span className="block">{formatDate(eventDetail.data.eventData.date)}</span>
                       </dd>
                     </div>
                   </dl>
@@ -190,17 +166,42 @@ function EventDetail() {
 
           <div className="mt-2 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
             {eventDetail.data.ticketData.map((ticket) => (
-              <button
-                key={ticket.id}
-                onClick={() => {
-                  if (ticket.status !== "Sold Out") {
-                    toggleModal(ticket.id);
-                  }
-                }}
-                disabled={ticket.status === "Sold Out"}
-                className="overflow-hidden rounded-lg bg-white shadow"
-              >
-                {isModalOpen && (
+              <div key={ticket.id}>
+                <button
+                  onClick={() => {
+                    if (ticket.status !== "Sold Out") {
+                      toggleModal(ticket.id);
+                    }
+                  }}
+                  disabled={ticket.status === "Sold Out"}
+                  className="overflow-hidden rounded-lg bg-white shadow w-full"
+                >
+                  <div className="p-5">
+                    <div className="flex items-center">
+                      <div className="w-0 flex-1">
+                        <dl>
+                          <dt className="truncate text-sm font-medium text-gray-500">
+                            {ticket.name} - {ticket.status}
+                          </dt>
+                          <dd>
+                            <div className="text-lg font-medium text-gray-900">
+                              {ticket.price}
+                            </div>
+                          </dd>
+                        </dl>
+                      </div>
+                      <div className="flex-shrink-0">
+                        {ticket.status === "Sold Out" ? (
+                          <XCircleIcon className="h-6 w-6 text-red-400" aria-hidden="true" />
+                        ) : (
+                          <CheckCircleIcon className="h-6 w-6 text-green-400" aria-hidden="true" />
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                </button>
+
+                {isModalOpen && selectedTicketId === ticket.id && (
                   <div
                     className="relative z-10"
                     aria-labelledby="modal-title"
@@ -218,13 +219,13 @@ function EventDetail() {
                                   xmlns="http://www.w3.org/2000/svg"
                                   fill="none"
                                   viewBox="0 0 24 24"
-                                  stroke-width="1.5"
+                                  strokeWidth="1.5"
                                   stroke="currentColor"
-                                  class="w-6 h-6"
+                                  className="w-6 h-6"
                                 >
                                   <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
                                     d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
                                   />
                                 </svg>
@@ -257,6 +258,7 @@ function EventDetail() {
                             <button
                               type="button"
                               className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                              onClick={() => setModalOpen(false)}
                             >
                               Batal
                             </button>
@@ -266,36 +268,7 @@ function EventDetail() {
                     </div>
                   </div>
                 )}
-                <div className="p-5">
-                  <div className="flex items-center">
-                    <div className="w-0 flex-1">
-                      <dl>
-                        <dt className="truncate text-sm font-medium text-gray-500">
-                          {ticket.name} - {ticket.status}
-                        </dt>
-                        <dd>
-                          <div className="text-lg font-medium text-gray-900">
-                            {ticket.price}
-                          </div>
-                        </dd>
-                      </dl>
-                    </div>
-                    <div className="flex-shrink-0">
-                      {ticket.status === "Sold Out" ? (
-                        <XCircleIcon
-                          className="h-6 w-6 text-red-400"
-                          aria-hidden="true"
-                        />
-                      ) : (
-                        <CheckCircleIcon
-                          className="h-6 w-6 text-green-400"
-                          aria-hidden="true"
-                        />
-                      )}
-                    </div>
-                  </div>
-                </div>
-              </button>
+              </div>
             ))}
           </div>
         </div>
@@ -334,22 +307,15 @@ function EventDetail() {
                 <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
                   <div>
                     <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
-                      <CheckIcon
-                        className="h-6 w-6 text-green-600"
-                        aria-hidden="true"
-                      />
+                      <CheckIcon className="h-6 w-6 text-green-600" aria-hidden="true" />
                     </div>
                     <div className="mt-3 text-center sm:mt-5">
-                      <Dialog.Title
-                        as="h3"
-                        className="text-base font-semibold leading-6 text-gray-900"
-                      >
+                      <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
                         Pemesanan Berhasil
                       </Dialog.Title>
                       <div className="mt-2">
                         <p className="text-sm text-gray-500">
-                          Pemesanan tiket konser{" "}
-                          {eventDetail.data.eventData.event_name} berhasil
+                          Pemesanan tiket konser {eventDetail.data.eventData.event_name} berhasil
                           ditambahkan, silakan cek pada laman pemesanan untuk
                           melanjutkan pembayaran.
                         </p>
@@ -359,7 +325,7 @@ function EventDetail() {
                   <div className="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
                     <button
                       type="button"
-                      className="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:col-start-2"
+                      className="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500"
                       onClick={() => {
                         setSuccessDialogOpen(false);
                         router.push("/orders");
@@ -369,7 +335,7 @@ function EventDetail() {
                     </button>
                     <button
                       type="button"
-                      className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0"
+                      className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
                       onClick={() => setSuccessDialogOpen(false)}
                       ref={cancelButtonRef}
                     >
